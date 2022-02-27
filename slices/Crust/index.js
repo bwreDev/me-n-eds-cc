26fd0c52a10fdfd28a634b1abbedffd02d8b387c
bwreDev <colton@bwre.tech>
2022-02-27 09:19:44 -0800

    Production bug testing

--- slices/Crust/index.js
+++ slices/Crust/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { RichText } from 'prismic-reactjs';
 
 const Crust = ({ slice }) => (
-  <section className='text-grey-200 bg-grey-900 max-w-2xl mx-auto px-2 lg:px-12 mb-4 lg:max-w-7xl'>
+  <section className='bg-grey-900 max-w-2xl mx-auto px-2 lg:px-12 mb-4 lg:max-w-7xl'>
     <div className='text-2xl font-extrabold text-red-500 border-b-2 border-red-500 mb-4 pb-2'>
       {slice.primary.title ? (
         <RichText render={slice.primary.title} />
@@ -16,7 +16,7 @@ const Crust = ({ slice }) => (
           <h3 className='text-lg pt-4 font-bold text-red-500'>
             <RichText render={item.crustName} />
           </h3>
-          <p className='px-2 lg:px-10 pt-2'>
+          <p className='text-grey-200 px-2 lg:px-10 pt-2'>
             <RichText render={item.crustDescription} />
           </p>
         </article>
