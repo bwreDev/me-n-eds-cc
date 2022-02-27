08c6f48a2a9888cd75d39a8f5b2d70736959c7c6
cbonner26118 <cbonner26118@gmail.com>
2022-02-26 16:52:12 -0800

    Removing unnecessary code for deployment.

--- .slicemachine/assets/slices/Crust/index.stories.js
+++
@@ -1,9 +0,0 @@
-import MyComponent from '../../../../slices/Crust';
-
-export default {
-  title: 'slices/Crust'
-}
-
-
-export const _DefaultSlice = () => <MyComponent slice={{"variation":"default-slice","name":"Default slice","slice_type":"crust","items":[{"crustName":[{"type":"heading3","text":"Optimize impactful models","spans":[]}],"crustDescription":[{"type":"paragraph","text":"Sit elit anim id ad laboris velit irure occaecat. Velit deserunt magna excepteur incididunt adipisicing proident non nisi cillum id excepteur et ex reprehenderit. Velit labore pariatur aliqua cillum occaecat ex tempor ea ullamco tempor laboris ut.","spans":[]}]},{"crustName":[{"type":"heading3","text":"Empower B2B systems","spans":[]}],"crustDescription":[{"type":"paragraph","text":"Excepteur aliquip qui proident anim irure quis cupidatat reprehenderit aliquip velit enim.","spans":[]}]},{"crustName":[{"type":"heading3","text":"Productize next-generation action-items","spans":[]}],"crustDescription":[{"type":"paragraph","text":"Dolor amet in voluptate.","spans":[]}]}],"primary":{"title":[{"type":"heading1","text":"Engineer dot-com mindshare","spans":[]}]},"id":"_DefaultSlice"}} />
-_DefaultSlice.storyName = 'Default slice'
