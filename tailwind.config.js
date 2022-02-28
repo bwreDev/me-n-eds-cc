f2da69344b224c2d3593520a6bef8e3f74e94b28
bwreDev <colton@bwre.tech>
2022-02-28 09:02:27 -0800

    Updated Tailwind purge proccess

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,28 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-	purge: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
-		'./components/**/*.{js,ts,jsx,tsx}',
-	],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		colors: {
-			warmGrey: colors.gray,
-			grey: colors.coolGray,
-			red: colors.red,
-		},
-		extend: {
-			backgroundImage: {
-				'offset-315': 'url(/images/offset-315.jpg)',
-				'grunge-overlay': 'url(/images/sm-md-grunge-black-66.png)',
-				'blast-overlay': 'url(/images/blast-flame-black.png)',
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './slices/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      warmGrey: colors.gray,
+      grey: colors.coolGray,
+      red: colors.red,
+    },
+    extend: {
+      backgroundImage: {
+        'offset-315': 'url(/images/offset-315.jpg)',
+        'grunge-overlay': 'url(/images/sm-md-grunge-black-66.png)',
+        'blast-overlay': 'url(/images/blast-flame-black.png)',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
 };
